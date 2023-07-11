1b3bc88640b723face25be4fb4db9067757c004e
Kusuma <119119194+ChyKusuma@users.noreply.github.com>
2023-07-12 01:46:25 +0700

    Update Block.hpp
    Signed-off-by: Kusuma <119119194+ChyKusuma@users.noreply.github.com>

--- Block.hpp
+++ Block.hpp
@@ -18,13 +18,13 @@
 #pragma once
 
 #include <stdexcept>
-#include <fstream>
+#include <fstream> 
 #include <iostream>
-#include <stdexcept>
+#include <ctime>
 #include <string>
 #include <vector>
-#include <ctime>
 
+#include "Block.hpp"
 #include "Hash.hpp"
 #include "Sign.hpp"
 #include "json.hpp"
@@ -32,54 +32,73 @@
 #include "Chain.hpp"
 #include "PoW.hpp"
 #include "db.hpp"
-#include "verify.hpp"
-
-using json = nlohmann::json;
+#include "Verify.hpp"
 
 namespace SPHINXVerify {
     class SPHINX_PublicKey {
     public:
-        // Placeholder definition for SPHINX_PublicKey
+        std::string publicKey;  // Placeholder for the public key
+
+        // Constructor
+        SPHINX_PublicKey(const std::string& key) : publicKey(key) {}
     };
 
+    // Function to verify the signature of a block
     bool verifySignature(const std::string& blockHash, const std::string& signature, const SPHINX_PublicKey& publicKey);
 }
 
 namespace SPHINXHash {
+    // Function to calculate the SPHINX-256 hash of data
     std::string SPHINX_256(const std::string& data);
 }
 
 namespace SPHINX_Chain {
     class Chain {
     public:
+        // Function to add a block to the chain
         void addBlock(const SPHINXMerkleBlock::MerkleBlock& block);
-    };
-}
-
-namespace SPHINXMerkleBlock {
-    class MerkleBlock {
-    public:
-        std::string constructMerkleTree(const std::vector<SignedTransaction>& signedTransactions) const;
-
-        bool verifyMerkleRoot(const std::string& merkleRoot, const std::vector<SignedTransaction>& transactions) const;
-
-        bool verifyMerkleRoot(const std::string& merkleRoot, const std::vector<SignedTransaction>& signedTransactions) const;
     };
 }
 
 namespace SPHINXDb {
     class DistributedDb {
     public:
+        // Function to save data with a specific block hash
         void saveData(const std::string& data, const std::string& blockHash);
 
+        // Function to load data for a given block ID
         std::string loadData(const std::string& blockId);
+    };
+}
+
+namespace SPHINXMerkleBlock {
+    class MerkleBlock {
+    public:
+        // Function to construct the Merkle tree
+        std::string constructMerkleTree(const std::vector<SPHINXTrx::Transaction>& signedTransactions) const;
+
+        // Function to verify the Merkle root
+        bool verifyMerkleRoot(const std::string& merkleRoot, const std::vector<SPHINXTrx::Transaction>& signedTransactions) const;
     };
 }
 
 namespace SPHINXBlock {
     class Block {
+    private:
+        std::string previousHash_;
+        std::string merkleRoot_;
+        std::string signature_;
+        uint32_t blockHeight_;
+        std::time_t timestamp_;
+        uint32_t nonce_;
+        uint32_t difficulty_;
+        std::vector<std::string> transactions_;
+        SPHINX_Chain::Chain* blockchain_;
+
     public:
         Block(const std::string& previousHash);
+
+        std::string calculateBlockHash() const;
 
         void addTransaction(const std::string& transaction);
 
@@ -90,8 +109,6 @@ namespace SPHINXBlock {
         bool verifyBlock(const SPHINXVerify::SPHINX_PublicKey& publicKey) const;
 
         bool verifySignature(const SPHINXVerify::SPHINX_PublicKey& publicKey) const;
-
-        std::string calculateBlockHash() const;
 
         void setBlockHeight(uint32_t height);
 
@@ -105,6 +122,7 @@ namespace SPHINXBlock {
 
         void addToBlockchain();
 
+        // Block headers
         json toJson() const;
 
         void fromJson(const json& blockJson);
@@ -115,20 +133,8 @@ namespace SPHINXBlock {
 
         bool saveToDatabase(SPHINXDb::DistributedDb& distributedDb) const;
 
-    private:
-        std::string previousHash_;
-        std::string merkleRoot_;
-        std::string signature_;
-        uint32_t blockHeight_;
-        std::time_t timestamp_;
-        uint32_t nonce_;
-        uint32_t difficulty_;
-        std::vector<std::string> transactions_;
-        SPHINX_Chain::Chain* blockchain_;
+        static Block loadFromDatabase(const std::string& blockId, SPHINXDb::DistributedDb& distributedDb);
     };
-
-    const uint32_t MAX_BLOCK_SIZE = 1000;
-    const std::time_t MAX_TIMESTAMP_OFFSET = 600;
 }
 
 #endif  // BLOCK_HPP
